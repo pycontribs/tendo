0f35ac70286507433fb7e9f8b1bd81ebf467be5c
Triangle717 <le717@hotmail.com>
2013-07-08 12:31:19 -0400

    Update __init__.py
    Fixed relative import on Python 3k

--- tendo/__init__.py
+++ tendo/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Sorin Sbarnea"
 __copyright__ = "Copyright 2010-2013, Sorin Sbarnea"
 __email__ = "sorin(dot)sbarnea(at)gmail.com"
 __status__ = "Production"
-from version import __version__, __date__
+from .version import __version__, __date__
 __date__ = "2013-03-22"
 
 __all__ = ['tee', 'colorer', 'unicode', 'execfile2', 'singleton', 'ansiterm']
