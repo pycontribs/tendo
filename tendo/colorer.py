3989b441d0197dc7874c59367f4c9081f1b24cdd
Marco Emilio Poleggi <marco.poleggi@unige.ch>
2014-06-04 13:42:02 +0200

    Fix for Issue #5

--- tendo/colorer.py
+++ tendo/colorer.py
@@ -122,7 +122,7 @@ if (hasattr(sys.stderr, "isatty") and sys.stderr.isatty()) or \
                 color = '\x1b[0m'  # normal
             try:
                 new_args[1].msg = color + unicode(new_args[1].msg) + '\x1b[0m'  # normal
-            except e:
+            except Exception as e:
                 print type(new_args[1].msg)
                 raise e
             # print "after"
