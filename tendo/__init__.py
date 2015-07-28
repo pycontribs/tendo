1be0b074cc20316158c52b91e76562601c0e876a
Sorin Sbarnea <sorin.sbarnea@citrix.com>
2015-07-28 16:59:45 +0100

    Sorting out E402 with PEP8.

--- tendo/__init__.py
+++ tendo/__init__.py
@@ -2,20 +2,19 @@
 # Licensed to PSF under a Contributor Agreement.
 # See http://www.python.org/psf/license for licensing details.
 from __future__ import absolute_import
-
-__author__ = "Sorin Sbarnea"
-__copyright__ = "Copyright 2010-2013, Sorin Sbarnea"
-__email__ = "sorin(dot)sbarnea(at)gmail.com"
-__status__ = "Production"
 from .version import __version__
 import sys
-__date__ = "2015-07-28"
 
+__author__ = "Sorin Sbarnea"
+__copyright__ = "Copyright 2010-2015, Sorin Sbarnea"
+__email__ = "sorin.sbarnea@gmail.com"
+__status__ = "Production"
+__date__ = "2015-07-28"
 __all__ = ['tee', 'colorer', 'unicode',
            'execfile2', 'singleton', 'ansiterm', 'version']
 
 """
-Tendo is tested with Python 2.5-3.3
+Tendo is tested with Python 2.5-3.4
 """
 
 if sys.hexversion < 0x02050000:
