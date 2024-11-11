334ea313d8c566c0e0795ae9f1c38f6bd0475003
Sorin Sbarnea <ssbarnea@redhat.com>
2024-11-11 12:38:14 +0000

    Replace sphinx with mkdocs for documentation

--- docs/make.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import os
-
-if "PYTHONPATH" in os.environ:
-    os.environ["PYTHONPATH"] = "..:" + os.environ["PYTHONPATH"]
-else:
-    os.environ["PYTHONPATH"] = ".."
-os.system("make html")
